ae4be5db020f4210614eb9e15c9364ea9cd20a2e
or-sawa <s.seisaku.co@icloud.com>
2022-12-09 11:09:45 +0900

    first commit

---
+++ contact.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+
+<?php
+
+// 変数の初期化
+$page_flag = 0;
+
+if( !empty($_POST['btn_confirm']) ) {
+
+	$page_flag = 1;
+
+} elseif( !empty($_POST['btn_submit']) ) {
+
+  $page_flag = 2;
+
+}
+?>
+
+
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="株式会社Sawaは、ただのエージェントではなく、キャリアをデザインするデザインカンパニーです。「働くを夢中に。」という理念のもと、本質的な価値を創り続けます。">
+  <meta name="keywords" content="株式会社Sawa,求人,転職,鹿児島,保育園,介護,看護,個室ジム,パーソナルトレーナー">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="contact.css">
+  <link rel="icon" href="">
+  <meta name="theme-color" content="#ffffff">
+  <meta property="og:title" content="株式会社Sawa">
+  <meta property="og:site_name" content="株式会社Sawa">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="株式会社Sawaは、ただのエージェントではなく、キャリアをデザインするデザインカンパニーです。「働くを夢中に。」という理念のもと、本質的な価値を創り続けます。">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <title> 派遣依頼 | 株式会社Sawa</title>
+</head>
+<body class>
+  <main>
+    <div class="l-contents">
+      <div class="l-contents__inner l-inner">
+        <article class="p-entry l-primary">
+          <header class="p-entry l-primary">
+            <h1 class="p-entry__title" style="font-size: 34px;">
+              企業向け | 派遣依頼フォーム
+            </h1>
+          </header>
+          <div class="p-entry__body">
+            <p>
+              当WEBサイトでは、企業様からの派遣スタッフをご依頼いただくための入り口として、
+              派遣スタッフ依頼フォームを設けております。
+              <br>
+              以下のフォームにご明記の上、送信ボタンを押してください。
+            </p>
+            <p>
+              通常、3営業日以内のご返信を差し上げますが1週間経ちましたても返信がない場合は、送信トラブルの可能性がございますので、
+              その際はお手数でございますが、株式会社Sawa(099-297-6922)までお問い合わせくださいませ。
+            </p>
+
+            <?php if( $page_flag === 1 ): ?>
+
+            <h1 style="text-align: center; font-size: 20px; margin-top: 10px; margin-bottom: 25px;">入力内容の確認</h1>
+
+            <form method="post" action="reception.php" style="text-align: center;">
+              <div class="element_wrap">
+                <label>企業名</label>
+                <p><?php echo $_POST['your-name']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>企業名(フリガナ）</label>
+                <p><?php echo $_POST['text-263']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>担当者様お名前</label>
+                <p><?php echo $_POST['text-264']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>お電話番号</label>
+                <p><?php echo $_POST['tel-248']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>メールアドレス</label>
+                <p><?php echo $_POST['your-email']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>会社ご住所</label>
+                <p><?php echo $_POST['textarea-591']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>派遣希望の職種</label>
+                <p><?php echo $_POST['text-266']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>派遣依頼の内容</label>
+                <p><?php echo $_POST['textarea-592']; ?></p>
+              </div>
+              <div class="element_wrap">
+                <label>入力内容のチェック</label>
+                <p><?php echo $_POST['acceptance-503']; ?></p>
+              </div>
+
+              <a href="contact.php">戻る</a>
+              <input type="submit" name="btn_submit" value="送信">
+              <input type="hidden" name="your-name" value="<?php echo $_POST['your-name']; ?>">
+              <input type="hidden" name="text-263" value="<?php echo $_POST['text-263']; ?>">
+              <input type="hidden" name="text-264" value="<?php echo $_POST['text-264']; ?>">
+              <input type="hidden" name="tel-248" value="<?php echo $_POST['tel-248']; ?>">
+              <input type="hidden" name="your-email" value="<?php echo $_POST['your-email']; ?>">
+              <input type="hidden" name="textarea-591" value="<?php echo $_POST['textarea-591']; ?>">
+              <input type="hidden" name="text-266" value="<?php echo $_POST['text-266']; ?>">
+              <input type="hidden" name="textarea-592" value="<?php echo $_POST['textarea-592']; ?>">
+              <input type="hidden" name="acceptance-503" value="<?php echo $_POST['acceptance-503']; ?>">
+            </form>
+
+            <?php elseif( $page_flag === 2 ): ?>
+
+            <p>送信が完了しました。</p> 
+
+            <?php else: ?>
+
+            <div role="form" class="wpcf7" id="wpcf7-f1176-p1177-o1" lang="ja" dir="ltr">
+              <div class="screen-reader-response">
+                <p role="status" aria-live="polite" aria-atomic="true"></p>
+                <ul></ul>
+              </div>
+              <form action="" method="post" class="wpcf7-form init">
+                <div style="display: none;">
+
+                </div>
+                <p>
+                  <label>
+                    企業名
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="your-name">
+                      <input type="text" name="your-name" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    企業名(フリガナ)
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="text-263">
+                      <input type="text" name="text-263" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    担当者様お名前
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="text-264">
+                      <input type="text" name="text-264" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    お電話番号
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="tel-248">
+                      <input type="text" name="tel-248" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    メールアドレス
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="your-email">
+                      <input type="text" name="your-email" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    会社ご住所
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="textarea-591">
+                      <input type="text" name="textarea-591" cols="40" rows="10" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    派遣希望の職種
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="text-266">
+                      <input type="text" name="text-266" value size="40" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    派遣依頼の内容などをご記入ください。
+                    <br>
+                    <span class="wpcf7-form-control-wrap" data-name="textarea-592">
+                      <input type="text" name="textarea-592" cols="40" rows="10" class="wpcf7-form-control wpcf7-text wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+                    </span>
+                  </label>
+                </p>
+                <p>
+                  <span class="wpcf7-form-control-wrap" data-name="acceptance-503">
+                    <span class="wpcf7-form-control wpcf7-acceptance">
+                      <span class="wpcf7-list-item">
+                        <input type="checkbox" name="acceptance-503" value="1" aria-invalid="false">
+                      </span>
+                    </span>
+                  </span>
+                  内容にお間違いがなければ、チェックをお願いします。
+                </p>
+                <p>
+                  <input type="submit" value="入力内容を確認" class="wpcf7-form-control has-spinner wpcf7-submit" name="btn_confirm">
+                  <span class="wpcf7-spinner">
+
+                  </span>
+                </p>
+              </form>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  </main>
+
+  <?php endif; ?>
+
+  <footer class="front_footer">
+    <small class="front_footer-copyright">
+       Copyright ©︎ 2022 株式会社Sawa. 
+    </small>
+  </footer>
+</body>
+</html>
---
+++ reception.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="株式会社Sawaは、ただのエージェントではなく、キャリアをデザインするデザインカンパニーです。「働くを夢中に。」という理念のもと、本質的な価値を創り続けます。">
+  <meta name="keywords" content="株式会社Sawa,求人,転職,鹿児島,保育園,介護,看護,個室ジム,パーソナルトレーナー">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="contact.css">
+  <link rel="icon" href="">
+  <meta name="theme-color" content="#ffffff">
+  <meta property="og:title" content="株式会社Sawa">
+  <meta property="og:site_name" content="株式会社Sawa">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="株式会社Sawaは、ただのエージェントではなく、キャリアをデザインするデザインカンパニーです。「働くを夢中に。」という理念のもと、本質的な価値を創り続けます。">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <title> 受付完了 | 株式会社Sawa</title>
+</head>
+<body class>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark py-3 sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">
+          <h2 class="pt-2 text-white">TOPページに戻る</h2>
+        </a>
+      </div>
+    </nav>
+    <?php
+      mb_language("Japanese");
+      mb_internal_encoding("UTF-8");
+
+      $header = null;
+      $auto_reply_subject = null;
+      $auto_reply_text = null;
+      $auto_reply_subject = null;
+      $auto_reply_text = null;
+      date_default_timezone_set('Asia/Tokyo');
+
+      // ヘッダー情報を設定
+      $header = "MIME-Version: 1.0\n";
+      $header .= "From: Sawa Company <stakeyama32@gmail.com>\n";
+      $header .= "Reply-To: Sawa Company <stakeyama32@gmail.com>\n";
+
+      // 件名を設定
+      $auto_reply_subject = 'お問い合わせありがとうございます。';
+
+      // 本文を設定
+      $auto_reply_text = "この度は、お問い合わせ頂き誠にありがとうございます。
+      下記の内容でお問い合わせを受け付けました。\n\n";
+      $auto_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
+      $auto_reply_text .= "企業名：" . $_POST['your-name'] . "\n";
+      $auto_reply_text .= "企業名(フリガナ）：" . $_POST['text-263'] . "\n";
+      $auto_reply_text .= "担当者様お名前：" . $_POST['text-264'] . "\n";
+      $auto_reply_text .= "電話番号：" . $_POST['tel-248'] . "\n";
+      $auto_reply_text .= "メールアドレス：" . $_POST['your-email'] . "\n";
+      $auto_reply_text .= "住所：" . $_POST['textarea-591'] . "\n";
+      $auto_reply_text .= "派遣希望の職種：" . $_POST['text-266'] . "\n";
+      $auto_reply_text .= "派遣希望の内容：" . $_POST['textarea-592'] . "\n";
+      $auto_reply_text .= "入力内容のチェック：" . $_POST['acceptance-503'] . "\n\n";
+      $auto_reply_text .= "株式会社Sawa 事務局";
+
+      $to = $_POST['your-email'];
+
+      mb_send_mail($to, $auto_reply_subject, $auto_reply_text, $header);
+
+      // 運営側へ送るメールの件名
+      $admin_reply_subject = "問い合わせを受け付けました";
+      
+      // 本文を設定
+      $admin_reply_text = "下記の内容で問い合わせがありました。\n\n";
+      $admin_reply_text .= "問い合わせ日時：" . date("Y-m-d H:i") . "\n";
+      $admin_reply_text .= "企業名：" . $_POST['your-name'] . "\n";
+      $admin_reply_text .= "企業名(フリガナ)：" . $_POST['text-263'] . "\n";
+      $admin_reply_text .= "担当者様お名前：" . $_POST['text-264'] . "\n";
+      $admin_reply_text .= "電話番号：" . $_POST['tel-248'] . "\n";
+      $admin_reply_text .= "メールアドレス：" . $_POST['your-email'] . "\n";
+      $admin_reply_text .= "住所：" . $_POST['textarea-591'] . "\n";
+      $admin_reply_text .= "派遣希望の職種：" . $_POST['text-266'] . "\n";
+      $admin_reply_text .= "派遣希望の内容：" . $_POST['textarea-591'] . "\n";
+      $admin_reply_text .= "入力内容のチェック：" . $_POST['acceptance-503'] . "\n\n";
+
+      // 運営側へメール送信
+      mb_send_mail( 's.seisaku.co@icloud.com', $admin_reply_subject, $admin_reply_text, $header);
+    ?>
+    
+    <h1 style="text-align: center;">
+      受付完了しました！　担当者からの連絡をお待ちください。
+    </h1>
+  <footer class="front_footer">
+    <small class="front_footer-copyright">
+       Copyright ©︎ 2022 株式会社Sawa. 
+    </small>
+  </footer>
+</body>
+</html>
